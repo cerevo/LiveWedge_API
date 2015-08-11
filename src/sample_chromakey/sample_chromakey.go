0a161fb3bee77d4511eb790bbde7c47678d4afa1
Tetsuyuki Kobayashi <koba@cerevo.com>
2015-08-11 18:21:14 +0900

    sample_chromakey: Add sample_chromakey

---
+++ src/sample_chromakey/sample_chromakey.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"libvsw"
+	//"log"
+	"os"
+	"time"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "usge: %s IP_address_of_livewedge\n", os.Args[0])
+		os.Exit(1)
+	}
+	vsw := libvsw.NewVsw(os.Args[1])
+	wait := 4000
+
+	vsw.UploadFile("greenback.jpg")
+	vsw.SetChromaKey(libvsw.CHROMA_KEY_GREEN)
+	vsw.CutUs(1, 4)
+	time.Sleep(time.Duration(wait) * time.Millisecond)
+	vsw.CutUs(1, 0)
+
+	vsw.UploadFile("blueback.jpg")
+	vsw.SetChromaKey(libvsw.CHROMA_KEY_BLUE)
+	vsw.CutUs(1, 4)
+	time.Sleep(time.Duration(wait) * time.Millisecond)
+	vsw.CutUs(1, 0)
+
+	vsw.UploadFile("purpleback.jpg")
+	vsw.SetChromaKey(libvsw.CHROMA_KEY_PURPLE)
+	vsw.CutUs(1, 4)
+	time.Sleep(time.Duration(wait) * time.Millisecond)
+	vsw.CutUs(1, 0)
+
+	vsw.UploadFile("redback.jpg")
+	vsw.SetChromaKey(libvsw.CHROMA_KEY_RED)
+	vsw.CutUs(1, 4)
+	time.Sleep(time.Duration(wait) * time.Millisecond)
+	vsw.CutUs(1, 0)
+}
