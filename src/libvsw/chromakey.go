19060494721223f59a53ce2ecddcb4c782bf4813
Tetsuyuki Kobayashi <koba@cerevo.com>
2015-08-30 16:08:27 +0900

    libvsw: Change argument of SetChromaRange, not to use struct chroma_range
    And rename struct Chroma_rang to chroma_range to use only in chromakey.go.

--- src/libvsw/chromakey.go
+++ src/libvsw/chromakey.go
@@ -15,27 +15,27 @@ const (
 )
 
 // Color range of chroma key
-type Chroma_range struct {
+type chroma_range struct {
 	floor, ceil int
 }
 
-var chroma_range_table = []Chroma_range{
+var chroma_range_table = []chroma_range{
 	chroma_range_entry(0, 0, 8-1, 8-2),
 	chroma_range_entry(8+1, 0, 15, 8-2),
 	chroma_range_entry(8+1, 8+2, 15, 15),
 	chroma_range_entry(0, 8+2, 8-1, 15),
 }
 
-func chroma_range_entry(u0, v0, u1, v1 int) Chroma_range {
-	return Chroma_range{
+func chroma_range_entry(u0, v0, u1, v1 int) chroma_range {
+	return chroma_range{
 		(u0)<<4 | 1<<12 | (v0)<<20,
 		(u1)<<4 | 14<<12 | (v1)<<20,
 	}
 }
 
 // SetChromaRange sets color range of chroma key
-func (vsw Vsw) SetChromaRange(r Chroma_range) {
-	cmd := []uint32{SW_ID_SetChromaRange, uint32(r.floor), uint32(r.ceil)}
+func (vsw Vsw) SetChromaRange(floor, ceil int) {
+	cmd := []uint32{SW_ID_SetChromaRange, uint32(floor), uint32(ceil)}
 	send(vsw.conn, cmd)
 }
 
@@ -46,6 +46,6 @@ func (vsw Vsw) SetChromaKey(color int) {
 	if 0 > color || color >= CHROMA_KEY_NUM {
 		return
 	}
-	vsw.SetChromaRange(chroma_range_table[color])
+	vsw.SetChromaRange(chroma_range_table[color].floor, chroma_range_table[color].ceil)
 	vsw.SetSubMode(SUB_MODE_CHROMAKEY)
 }
